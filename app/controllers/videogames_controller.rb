cefe1625e1e5f0843a510e2c630e24984da12af8
David Poisson <zeraaiur@hotmail.com>
2018-06-10 13:06:12 -0400

    search api working

--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -1,26 +1,55 @@
 class VideogamesController < ApplicationController
+  require "open-uri"
+  require 'json'
   def index
     @videogames = Videogame.all
   end
 
   def show
-    @videogame = Videogame.find(params[:id])
+    # @videogame = Videogame.find(params[:id])
   end
 
   def new
+    @videogame = Videogame.new
   end
 
   def create
+    @videogame = Videogame.new
+
   end
 
+  def search
+    videogame = params[:videogame][:title]
+    response = open("https://api-endpoint.igdb.com/games/?search=#{videogame}",
+    {
+      "user-Key" => '241a39d7cb359e03ef5c0546f2798b09',
+      "Accept" => "application/json"
+    }).read
+
+    newgame = JSON.parse(response)
+
+    videogame_result = open("https://api-endpoint.igdb.com/games/#{newgame.first["id"]}",
+              {
+      "user-Key" => '241a39d7cb359e03ef5c0546f2798b09',
+      "Accept" => "application/json"
+    }).read
+
+    @result  = JSON.parse(videogame_result)
+        render :result
+
+
+
+
+
+
+  end
+
+
+
+
+
   def destroy
-
-
     @videogame = Videogame.find(params[:id])
     @videogame.destroy
-
-
-
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+    get 'videogames/result', to:"videogames#result", as: :result
+    post 'videogames/search' => "videogames#search", as: :search
+  get 'videogames/new', to: "videogames#new"
   get 'videogames', to: "videogames#index"
   get 'videogames/:id', to: "videogames#show"
-  get 'videogames/new', to: "videogames#new"
   post 'videogames', to: "videogames#create"
   delete 'videogames/:id', to: "videogames#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
